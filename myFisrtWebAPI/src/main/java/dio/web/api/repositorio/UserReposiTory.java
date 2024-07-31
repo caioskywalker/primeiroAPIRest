3e3fd2279b6226aa14088f0eed1eada81b772055
Caio Felipe <93288204+caioskywalker@users.noreply.github.com>
2024-07-31 01:10:58 -0300

    Melhorias na aplicação
    Utilizamos ArrayList para simular banco de dados e Stream para filtrar requisições de busca por id e nome, além de deletar usuários na lista em memória.

--- myFisrtWebAPI/src/main/java/dio/web/api/repositorio/UserReposiTory.java
+++ myFisrtWebAPI/src/main/java/dio/web/api/repositorio/UserReposiTory.java
@@ -1,40 +1,66 @@
 package dio.web.api.repositorio;
+
 import java.util.List;
 import java.util.ArrayList;
 
 import dio.web.api.model.Usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserReposiTory {
 
-    public void save(Usuario usuario){
-        if(usuario.getId()==null)
-            System.out.println("SAVE - Recebendo o usuário na camada de repositório");
-        else
-            System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+	List<Usuario> usuarioList = new ArrayList<Usuario>();
 
-        System.out.println(usuario);
-    }
-    public void deleteById(Integer id){
-        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
-        System.out.println(id);
-    }
-    public List<Usuario> findAll(){
-        System.out.println("LIST - Listando os usários do sistema");
-        List<Usuario> usuarios = new ArrayList<>();
-        usuarios.add(new Usuario("caio","farias"));
-        usuarios.add(new Usuario("frank","masterpass"));
-        return usuarios;
-    }
-    public Usuario findById(Integer id){
-        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
-        return new Usuario("caio","farias");
-    }
-    public Usuario findByUsername(String username){
-        System.out.println(String.format("FIND/username - Recebendo o usernamae: %s para localizar um usuário", username));
-        return new Usuario("gleyson","password");
-    }
+	public UserReposiTory(List<Usuario> usuarioList) {
+		this.usuarioList = usuarioList;
+	}
 
+	public void save(Usuario usuario) {
+		try {
+			if (usuario.getId() == null) {
+				throw new Exception("Usuario precisa de id");
+			} else {
+				usuarioList.add(usuario);
+				System.out.println("Usuario adicionado com sucesso");
+			}
+		} catch (Exception e) {
+			System.err.println("Erro ao adicionar usuário: " + e.getMessage());
+		}
+	}
+
+	public void deleteById(Integer id) {
+		Usuario userToRemove = usuarioList.stream().filter(u -> u.getId().equals(id)).findFirst().orElse(null);
+
+		try {
+			if (userToRemove.getId() == null) {
+				throw new Exception("Usuario precisa de id");
+			} else {
+				usuarioList.remove(userToRemove);
+			}
+		} catch (Exception e) {
+			System.err.println("Erro ao deletar usuário: " + e.getMessage());
+		}
+
+	}
+
+	public List<Usuario> findAll() {
+		System.out.println("LIST - Listando os usários do sistema");
+		return usuarioList;
+	}
+
+	public Usuario findById(Integer id) {
+		System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+		Usuario userEncontrado = usuarioList.stream().filter(u -> u.getId().equals(id)).findFirst().orElse(null);
+		return userEncontrado;
+	}
+
+	public Usuario findByUsername(String username) {
+		System.out.println(
+				String.format("FIND/username - Recebendo o usernamae: %s para localizar um usuário", username));
+		Usuario userEncontrado = usuarioList.stream().filter(u -> u.getLogin().equals(username)).findFirst().orElse(null);
+		return userEncontrado;
+	}
 
 }
